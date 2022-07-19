eb1e61acbd72cd7876be12006ebd9aadafa918c8
svenschilling <sven.schilling@gmail.com>
2022-07-19 20:00:45 +0200

    upload

---
+++ ArtGenerator.java
@@ -0,0 +1,318 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Random;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.TilePane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
+import javafx.stage.Stage;
+import javafx.scene.shape.SVGPath;
+import javafx.geometry.Insets;
+
+// needed to save the image
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.SnapshotParameters;
+import javafx.scene.image.Image;
+import javafx.scene.image.WritableImage;
+
+// shortcuts
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
+
+
+public class ArtGenerator extends Application {
+    ArrayList<Shape> makeShapes;
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        BorderPane root = new BorderPane();
+        HBox hBox = new HBox();
+        TilePane tilePane = new TilePane();
+        TextField textField = new TextField();
+        ScrollPane scrollPane = new ScrollPane(tilePane);
+        StackPane stackPane = new StackPane();
+
+        // vBox elements
+        Button buttonGenerateAdd = new Button("generate add");
+        Button buttonSaveImages = new Button("save images");
+        Button buttonReset = new Button("clear fields");
+        Button buttonDeleteSelected = new Button("delete selected");
+        Button buttonGenerateNew = new Button("generate new");
+        Button buttonNodeEditor = new Button("node editor");
+        
+        hBox.getChildren().addAll(textField,buttonGenerateNew,buttonGenerateAdd,buttonSaveImages,buttonDeleteSelected,buttonReset,buttonNodeEditor);
+            
+        //! build implementation of deleteSelected buttons
+        // button event -> delete selected graphics
+        buttonDeleteSelected.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("deleting selected items");
+                
+            }
+        });
+
+        // button event -> delete all images
+        buttonReset.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                try {
+                    makeShapes.clear();
+                    tilePane.getChildren().clear();
+                    textField.setText("");
+                } catch (Exception e) {
+                    e.getStackTrace();
+                }
+            }
+        });
+
+        // button event -> add generated art 
+        buttonGenerateAdd.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                int input;
+                try {
+                    input = Integer.parseInt(textField.getText());    
+                    makeShapes = createShape(input);
+                    tilePane.getChildren().addAll(makeShapes);
+                } catch (Exception e) {
+                    e.getStackTrace();
+                }
+            } 
+        });
+
+        //! doesnt work when nothing was generated before 
+        buttonGenerateNew.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                int input;
+                // reset array and tilepane from shapes
+                makeShapes.clear();
+                tilePane.getChildren().clear();
+                try {
+                    input = Integer.parseInt(textField.getText());    
+                    makeShapes = createShape(input);
+                    tilePane.getChildren().addAll(makeShapes);
+                } catch (Exception e) {
+                    e.getStackTrace();
+                }
+            } 
+        });
+        
+        //! when i save with too much (~300) images genereted it throws an save error
+        buttonSaveImages.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                WritableImage image = tilePane.snapshot(new SnapshotParameters(), null);
+                
+                LocalDateTime localDateTime = LocalDateTime.now();
+                DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy_HH-mm-ss");
+                String formattedDate = localDateTime.format(myFormatObj);
+                
+                File file = new File("art_template" + formattedDate + ".png");
+                                
+                try {
+                    ImageIO.write(SwingFXUtils.fromFXImage(image, null), "png", file);    
+                } catch (Exception e) {
+                    e.getStackTrace();
+                }      
+            } 
+        });
+
+
+        //! open node editor
+        buttonNodeEditor.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                // TODO Auto-generated method stub
+                
+            }
+        });
+        
+        //! show a little number in the upper top corner of each box
+        //! adjust padding
+        // tilepane elements
+        int tileWidth = 50;
+        int tileHeight = 50;
+        tilePane.setPadding(new Insets(10, 10, 10, 10));
+        tilePane.setPrefSize(tileWidth, tileHeight);
+        
+        // stage settings
+        primaryStage.setTitle("algorithm prototype");
+              
+        Scene scene = new Scene(root, 600, 600);
+        
+
+        //! setup scrollpane that it format the images around the whole horizontal length
+        scrollPane.setPrefWidth(400);
+        System.out.println(scrollPane.widthProperty().getValue());
+    
+        primaryStage.getIcons().add(new Image(ArtGenerator.class.getResourceAsStream("paintbrush-icon.png")));
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        
+        // key shortcuts
+        //! set shortcut to generate button -> ENTER
+        scene.addEventFilter(KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>() {
+            public void handle(KeyEvent ke) {
+                if (ke.getCode() == KeyCode.ENTER) {
+                    System.out.println("Key Pressed: " + ke.getCode());
+                    ke.consume(); // <-- stops passing the event to next node
+                }
+            }
+        });
+        
+        // root elements     
+        root.setTop(hBox);
+        root.setCenter(scrollPane);
+
+
+        
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    public static int random_int(int Min, int Max)
+    {
+         return (int) (Math.random()*(Max-Min))+Min;
+    }
+    
+    public ArrayList<Shape> createShape(int numberOfShapes) {
+        ArrayList<Shape> shapeList = new ArrayList<>();
+        ArrayList<Shape> mergedShapeList = new ArrayList<>();
+       
+        
+        Random random = new Random();
+        
+        int randomRadius, 
+            randomWidth,  
+            randomHeight, 
+            randomShapeOperation,
+            randomMergedShapeOperation,
+            randomRotation,
+            randomShape1,
+            randomShape2;
+
+        double randomScaleX,
+               randomScaleY; 
+        
+        int condition = 0;
+        int iterations = 10;
+        int counter = 0;
+
+        // needs times of iterations the amount of shapes
+        for (int i = 0; i < numberOfShapes * iterations; i++) {
+            randomShapeOperation = random_int(1, 4);
+            randomShape1 = random_int(1, 4);
+            randomShape2 = random_int(1, 4);
+            randomWidth = random_int(20, 60);
+            randomHeight = random_int(20, 70);
+            randomRadius = random_int(20, 90);
+            randomRotation = random_int(0, 180);
+            
+            //! ho deeper into configuration of shapes + make use of other like bezier curve
+            // triangle
+            SVGPath triangle = new SVGPath();      
+            //! make these creation numbers also random 
+            String path = "M 100 100 L 300 100 L 200 300 z";  
+            triangle.setContent(path);  
+            
+            Circle circle = new Circle(randomRadius, Color.BLACK);
+            Rectangle rectangle = new Rectangle(randomWidth, randomHeight, Color.BLACK);
+            
+            // shape operations
+            //! pick randomly circle or rectangle  
+            //! its creating empty spaces that shouldnt exists!
+            switch (randomShapeOperation) {
+                case 3:
+                    Shape substractShape = Shape.subtract(circle, rectangle);
+                    substractShape.setRotate(randomRotation);
+                    substractShape.setScaleX(randomScaleX = random.nextDouble());
+                    substractShape.setScaleY(randomScaleY = random.nextDouble());
+                    
+                    shapeList.add(substractShape);
+                    break;
+                case 2:
+                    Shape unionShape = Shape.union(circle, rectangle);
+                    unionShape.setRotate(randomRotation);
+                    unionShape.setScaleX(randomScaleX = random.nextDouble());
+                    unionShape.setScaleY(randomScaleY = random.nextDouble());
+
+                    shapeList.add(unionShape);
+                    break;
+                case 1:
+                    Shape intersectShape = Shape.intersect(circle, rectangle);
+                    intersectShape.setRotate(randomRotation);
+                    intersectShape.setScaleX(randomScaleX = random.nextDouble());
+                    intersectShape.setScaleY(randomScaleY = random.nextDouble());
+        
+                    shapeList.add(intersectShape);
+                    break;   
+            }
+        }
+        
+        for (int i = 0; i < numberOfShapes; i++) {      
+            do { 
+                randomShapeOperation = random_int(1, 4);
+                randomRotation = random_int(0, 360);
+
+                switch (randomShapeOperation) {
+                    case 3:
+                        Shape substractShape = Shape.subtract(shapeList.get(counter), shapeList.get(counter+1));
+                        substractShape.setRotate(randomRotation);
+                        // substractShape.setScaleX(randomScaleX = random.nextDouble());
+                        // substractShape.setScaleY(randomScaleY = random.nextDouble());
+
+                        mergedShapeList.add(substractShape);
+                        break;
+                    case 2:
+                        Shape unionShape = Shape.union(shapeList.get(counter), shapeList.get(counter+1));
+                        unionShape.setRotate(randomRotation);
+                        //unionShape.setScaleX(randomScaleX = random.nextDouble());
+                        //unionShape.setScaleY(randomScaleY = random.nextDouble());
+
+                        mergedShapeList.add(unionShape);
+                        break;
+                    case 1:
+                        Shape intersectShape = Shape.intersect(shapeList.get(counter), shapeList.get(counter+1));
+                        intersectShape.setRotate(randomRotation);
+                        //intersectShape.setScaleX(randomScaleX = random.nextDouble());
+                        //intersectShape.setScaleY(randomScaleY = random.nextDouble());
+            
+                        mergedShapeList.add(intersectShape);
+                        break;           
+                }
+                condition++; 
+                counter++;
+            } while (condition == iterations * numberOfShapes);
+        }
+        
+        return mergedShapeList;
+    }
+    
+    
+    // mark selected with a black border
+    public void markSelectedGraphics () {
+        //! build mouse event that pick ups marked numbers and then show a border around them    
+    }
+}
---
+++ TODO.md
@@ -0,0 +1,62 @@
++  scan svg path with javafx 2d shapes
++  user can enter a cell size which is used when saving the document
+
++ doubleclick on selected art to maximes it
+    + have an clickable x to return to overview
++ textfield can only take integer
+    + else : show on label or popup window that you have input the wrong type COLOR whole hbox red when its unequal int
+
+### features
+    - save selection as svg or png or jpg
+    - save selected items as single images
+        + define resolution for them to export those
+    - edit nodes where those shapes center around
+
+
+### generate algorithm
+ - 3-10 random iteration with random selection of: union, subtract, intersect
+ - mirror part of it
+ - have nodes at specific x,y areas where those iterations will take place to make it always look for example : humanoid, creature ..
+ - train ml on it
+ #### algo 2
+ - generate svg file build after ref with random numbers 
+
+ ## extra
+ - generate shapes along a path (humanoid) , they are stored in a folder and can
+ - make the create method a strategy pattern
+
+ ### shape generation by 3d shapes
+ - user  can put own 3d shape into 3d_shapes directory to use them
+
+### shape-editor // node ediot
+- a single window where the user can create a random shape by pressing buttons or moving already existing shapes with the mouse (and then generate new ones over the moved ones)
+- manipulation methods like flip(), mirrorX(), mirrorY(), intersectGroups(), substractGroups(), unionGroups(),
+- generate shapes around specific nodes and lines where they are connected from
+- let people make prefabs and save them to a list that is reachable within normal creation window
+
+## architecture 
+- make shapes to interfaces
+
+res:
+https://github.com/karldd/Alchemy/ !
+https://github.com/Climenty/svg-join
+https://www.demo2s.com/java/javafx-combining-shapes.html
+https://edencoding.com/svg-javafx/
+https://www.w3.org/Graphics/SVG/   => https://www.w3.org/TR/SVG11/
+https://www.hameister.org/JavaFX_SVGPath.html
+https://docs.oracle.com/javafx/2/canvas/jfxpub-canvas.htm
+https://stackoverflow.com/questions/21973777/easiest-way-to-read-in-svg-path-data-with-java !
+https://github.com/karldd/Alchemy/blob/master/src/org/alchemy/create/PullShapes.java !
+https://stackoverflow.com/questions/38994519/responsive-ui-design-with-a-tilepane-in-a-scrollpane !
+
+https://mavenlibs.com/jar/file/org.apache.xmlgraphics/batik-svggen
+
+ref:
+https://80.lv/articles/brainstorm-an-easier-way-to-come-up-with-ideas/
+https://slick.ninjacave.com/javadoc/  !deprecated
+https://libgdx.com/
+https://stackoverflow.com/questions/33961606/javafx-how-to-drag-object-along-given-svg-path-like-a-slider
+
+useful libs:
+https://xmlgraphics.apache.org/batik/using/
+https://www.jfree.org/jfreesvg/javadoc/org.jfree.svg/org/jfree/svg/package-summary.html
